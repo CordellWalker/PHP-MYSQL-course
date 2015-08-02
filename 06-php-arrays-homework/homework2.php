21ac89187349f4b042e787f545979b79298df28f
Krasi <wwwdabeda@abv.bg>
2015-08-02 17:52:44 +0300

    2ра задача от домашното в четвъртък. Не напълно изпълняваща условието

---
+++ 06-php-arrays-homework/homework2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Homework 2</title>
+</head>
+<body>
+<?php
+    $userArr = array();
+
+	$userArr[] = array('Soft Intellect'=>'http://soft-intellect.com/');
+    $userArr[] = array('AБВ'=>'http://www.abv.bg/');
+    $userArr[] = array('Mtel'=>'http://www.mtel.bg/');
+    $userArr[] = array('Google' =>'https://www.google.bg/');
+    $userArr[] = array('ЧЕЗ'=>'http://www.cez.bg/bg/home.html');
+
+	
+    
+foreach($userArr as $var) 
+{
+    foreach($var as $key=>$value)
+    {
+        echo "$key:  $value";
+        echo "<br>";
+    }
+}
+  
+?>
+
+</body>
+</html>
